05412d72926cd621abfe3fb37114f992228e4f94
ahadrez <ahadriz@gmail.com>
2025-04-03 01:43:00 +0300

    Update init.sh

--- .devspace-init/init.sh
+++ .devspace-init/init.sh
@@ -6,6 +6,3 @@ alias gauth="gcloud auth login --no-launch-browser --update-adc"
 alias gproject="gcloud config set project"
 alias ginfo="gcloud info"
 EOF
-
-# Source .bashrc if it exists (some systems rely on this chaining)
-grep -q ".bashrc" ~/.bash_profile || echo '[ -f ~/.bashrc ] && source ~/.bashrc' >> ~/.bash_profile
