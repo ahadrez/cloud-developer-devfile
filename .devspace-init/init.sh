67ac1629c12c49db9be80347642f068e463d195f
ahadrez <ahadriz@gmail.com>
2025-04-03 15:50:08 +0300

    Update init.sh

--- .devspace-init/init.sh
+++ .devspace-init/init.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-# Add aliases to .bash_profile
-cat <<EOF >> ~/.bash_profile
+echo "Updating ~/.bashrc with gcloud aliases..."
+
+cat <<EOF >> ~/.bashrc
+
+# gcloud aliases
 alias gcloud_auth="gcloud auth login --no-launch-browser --update-adc"
 alias gcloud_project="gcloud config set project"
 alias gcloud_info="gcloud info"
 EOF
+
+echo "Done updating ~/.bashrc"
